af8091738b415f5b4681fa9d778c262e53318ee5
Nam Nguyen <nguy1053@msu.edu>
2024-02-23 08:33:17 -0500

    Finalizing axios baseUrl

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -2,7 +2,8 @@ import axios from "axios"
 
 const axiosInstance = axios.create()
 
-axiosInstance.defaults.baseURL='http://localhost:3000'
+axiosInstance.defaults.baseURL='https://courseflowbe.onrender.com'
+// axiosInstance.defaults.baseURL='http://localhost:3000/'
 axiosInstance.defaults.headers.post['Content-Type'] = 'application/json'    
 
 export default axiosInstance
